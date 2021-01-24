f02d1a42ea412fd2991555e16b29c87c8019e203
Alexander Wilkinson <alexwilkinson@gmail.com>
2021-01-24 21:08:31 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Brew",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-MMYKHT4235",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
